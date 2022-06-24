b3dfb79efab0d2b4d9d7626f7b017f676717ab3d
Giulia <giuliafloris88@gmail.com>
2022-06-24 15:49:48 +0200

    Initial Commit

---
+++ RandomWikiPage/RandomWikiPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomWikiPageApp.swift
+//  RandomWikiPage
+//
+//  Created by user on 24/06/22.
+//
+
+import SwiftUI
+
+@main
+struct RandomWikiPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
